294bf1ace235dc3a2c72780fb16203f2d888a83e
Mykola Grybyk <xlenz7@gmail.com>
2016-02-23 20:03:38 +0200

    move out start container to service

--- src/app/containers/startContainer/startContainer.js
+++ src/app/containers/startContainer/startContainer.js
@@ -1,75 +1,93 @@
-angular.module( 'app.startContainer', [
-  'ui.router',
-  'ui.bootstrap'
-])
+angular.module('app.startContainer', [
+    'ui.router',
+    'ui.bootstrap'
+  ])
 
-.config( function config( $stateProvider ) {
-  var home = 'home';
-  $stateProvider
-    .state( 'startContainerMain', {
-      parent: home,
-      onEnter: function onEnter( $modal, $state ) {
-        $modal
+  .config(function config($stateProvider) {
+    var home = 'home';
+    $stateProvider
+      .state('startContainerMain', {
+        parent: home,
+        onEnter: function onEnter($modal, $state) {
+          $modal
           // handle modal open
-          .open({
-            // main view
-            templateUrl: 'containers/startContainer/startContainer.tpl.html',
-            controller: 'StartContainerCtrl'
-          })
-          .result.then( function() {
+            .open({
+              // main view
+              templateUrl: 'containers/startContainer/startContainer.tpl.html',
+              controller: 'StartContainerCtrl'
+            })
+            .result.then(function () {
             // after clicking OK button
             $state.transitionTo(home);
-          }, function() {
+          }, function () {
             // after clicking Cancel button or clicking background
             $state.transitionTo(home);
           });
-      }
-    })
-    .state('startContainer', {
-      url: 'containers/:name/start',
-      parent: 'startContainerMain',
-      // nestead views
-      views: {
-        "bindingAddress@": {
-          controller: 'BindingAddressCtrl',
-          templateUrl: 'containers/bindingAddress/bindingAddress.tpl.html'
         }
+      })
+      .state('startContainer', {
+        url: 'containers/:name/start',
+        parent: 'startContainerMain',
+        // nestead views
+        views: {
+          "bindingAddress@": {
+            controller: 'BindingAddressCtrl',
+            templateUrl: 'containers/bindingAddress/bindingAddress.tpl.html'
+          }
+        }
+      })
+    ;
+  })
+
+  .controller('StartContainerCtrl',
+    function StartContainerCtrl($scope, $stateParams, Cookies, ContainerService, Container, ContainerStartService) {
+
+      $scope.settings = Cookies.settings;
+
+      ContainerService.getByName(decodeURIComponent($stateParams.name))
+        .then(function (container) {
+          Container.get({id: container.Id}, function (info) {
+            $scope.container = info;
+          });
+        });
+
+      $scope.start = function () {
+
+        ContainerCreateService.start($scope.container.Id, function() {
+          $scope.$close();
+        });
+
+        //var startContainerParams = {
+        //  id: $scope.container.Id,
+        //  privileged: true
+        //};
+        //
+        //Container.start(startContainerParams, function () {
+        //  console.log('Container started.');
+        //  ContainerService.update();
+        //  $scope.$close();
+        //});
+      };
+
+      $scope.close = function () {
+        $scope.$dismiss();
+      };
+
+    })
+
+  .service('ContainerStartService',
+    function (Container, ContainerService) {
+      this.start = function (containerId, callback) {
+        Container.start({
+          id: containerId,
+          privileged: true
+        }, function () {
+          console.log('Container started.');
+          ContainerService.update();
+          if (callback) callback();
+        });
       }
     })
-  ;
-})
-
-.controller( 'StartContainerCtrl', 
-  function StartContainerCtrl( $scope, $stateParams, Cookies, ContainerService, Container, Config ) {
-
-  $scope.settings = Cookies.settings;
-
-  ContainerService.getByName( decodeURIComponent($stateParams.name) )
-    .then(function( container ) {
-      Container.get({ id: container.Id }, function( info ) {
-        $scope.container = info;
-      });
-  });
-
-  $scope.start = function() {
-
-    var startContainerParams = {
-      id: $scope.container.Id,
-      privileged: true
-    };
-
-    Container.start(startContainerParams, function() {
-      console.log('Container started.');
-      ContainerService.update();
-      $scope.$close();
-    });
-  };
-
-  $scope.close = function() {
-    $scope.$dismiss();
-  };
-
-})
 
 ;
 
