74011cde556980a201a50453fe9a1fe5b52fc3a2
Mykola Grybyk <mgrybyk@serena.com>
2017-07-03 19:18:39 +0300

    docker version bump

--- src/common/docker/docker.js
+++ src/common/docker/docker.js
@@ -2,7 +2,7 @@
 angular.module( 'docker', ['ngResource'] )
  
 .factory( 'Settings', function( $location ) {
-  var api = 'v1.27';
+  var api = 'v1.30';
   var DOCKER_HOST = $location.host();
   var DOCKER_PORT = '2375';
   // url to proxy server
