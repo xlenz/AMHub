b8696d8f32358631e51d562fe00cb3effb0e178e
Mykola Grybyk <mgrybyk@serena.com>
2017-11-21 16:39:51 +0200

    reformat, no changes

--- src/app/containers/containerInfo/containerInfo.js
+++ src/app/containers/containerInfo/containerInfo.js
@@ -1,91 +1,91 @@
-angular.module( 'app.containerInfo', [
+angular.module('app.containerInfo', [
   'ui.router',
   'ui.bootstrap'
 ])
 
-.config( function config( $stateProvider ) {
-  var home = 'home';
-  $stateProvider
-    .state( 'containerInfo', {
-      url: 'containers/:name',
-      parent: home,
-      onEnter: function onEnter( $uibModal, $state ) {
-        $uibModal
-          // handle modal open
-          .open({
-            templateUrl: 'containers/containerInfo/containerInfo.tpl.html',
-            controller: 'ContainerInfoCtrl',
-            size: 'lg'
-          })
-          .result.then( function() {
-            // after clicking OK button
-          }, function() {
-            // after clicking Cancel button or clicking background
-            $state.transitionTo(home);
-          });
-      }
-    })
-  ;
-})
-
-.controller( 'ContainerInfoCtrl', 
-  function ContainerInfoCtrl( $scope, $stateParams, $location, $interval, ContainerService, Container ) {
-
-  var intervalPromise; 
-  ContainerService.getByName( decodeURIComponent($stateParams.name) )
-    .then(function( container ) {
-      Container.get({ id: container.Id }, function( data ) {
-        $scope.container = data;
-        if(container.Status.indexOf('Up') != -1) {
-          intervalPromise = $interval(top, 5000);
-          top();
+  .config(function config ($stateProvider) {
+    var home = 'home'
+    $stateProvider
+      .state('containerInfo', {
+        url: 'containers/:name',
+        parent: home,
+        onEnter: function onEnter ($uibModal, $state) {
+          $uibModal
+            // handle modal open
+            .open({
+              templateUrl: 'containers/containerInfo/containerInfo.tpl.html',
+              controller: 'ContainerInfoCtrl',
+              size: 'lg'
+            })
+            .result.then(function () {
+              // after clicking OK button
+            }, function () {
+              // after clicking Cancel button or clicking background
+              $state.transitionTo(home)
+            })
         }
-      });
-  });
+      })
+  })
 
-  var top = function() {
-    Container.top({ id: $scope.container.Id }, function( data ) {
-      $scope.top = data;
-    });
-  };
+  .controller('ContainerInfoCtrl',
+    function ContainerInfoCtrl ($scope, $stateParams, $location, $interval, ContainerService, Container) {
+      var intervalPromise
+      ContainerService.getByName(decodeURIComponent($stateParams.name))
+        .then(function (container) {
+          Container.get({
+            id: container.Id
+          }, function (data) {
+            $scope.container = data
+            if (container.Status.indexOf('Up') != -1) {
+              intervalPromise = $interval(top, 5000)
+              top()
+            }
+          })
+        })
 
-  $scope.connectHTTP = function( ip, port ) {
-    if(ip == '0.0.0.0') {
-      ip = $location.host();
-    }
-    $scope.targeturl = "http://"+ip+":"+port;    
-  };
+      var top = function () {
+        Container.top({
+          id: $scope.container.Id
+        }, function (data) {
+          $scope.top = data
+        })
+      }
 
-  $scope.connectHTTPS = function( ip, port ) {
-    if(ip == '0.0.0.0') {
-      ip = $location.host();
-    }
-    $scope.targeturl = "https://"+ip+":"+port;   
-  };
+      $scope.connectHTTP = function (ip, port) {
+        if (ip == '0.0.0.0') {
+          ip = $location.host()
+        }
+        $scope.targeturl = 'http://' + ip + ':' + port
+      }
 
-  $scope.connectRDP = function( ip, port ) {
-    if(ip == '0.0.0.0') {
-      ip = $location.host();
-    }
-    var blob = 
-      new Blob(["auto connect:i:1\n"+
-                "full address:s:"+ip+":"+port+"\n"+
-                "username:s:user\n"+
-                "redirectclipboard:i:1"],
-      {type: "text/plain;charset=" + document.characterSet});
-    // FileSaver.js
-    saveAs(blob, $scope.container.Config.Hostname+".rdp");
-  };
+      $scope.connectHTTPS = function (ip, port) {
+        if (ip == '0.0.0.0') {
+          ip = $location.host()
+        }
+        $scope.targeturl = 'https://' + ip + ':' + port
+      }
 
-  $scope.close = function() {
-    $scope.$dismiss();
-  };
+      $scope.connectRDP = function (ip, port) {
+        if (ip == '0.0.0.0') {
+          ip = $location.host()
+        }
+        var blob =
+          new Blob(['auto connect:i:1\n' +
+            'full address:s:' + ip + ':' + port + '\n' +
+            'username:s:user\n' +
+            'redirectclipboard:i:1'
+          ], {
+            type: 'text/plain;charset=' + document.characterSet
+          })
+        // FileSaver.js
+        saveAs(blob, $scope.container.Config.Hostname + '.rdp')
+      }
 
-  $scope.$on('$destroy', function () { 
-    $interval.cancel(intervalPromise);
-  });
+      $scope.close = function () {
+        $scope.$dismiss()
+      }
 
-})
-
-;
-
+      $scope.$on('$destroy', function () {
+        $interval.cancel(intervalPromise)
+      })
+    })
