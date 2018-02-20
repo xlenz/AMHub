51632bf557691c6a75eb4afd7aaea623662efcc8
Mykola Grybyk <mgrybyk@serena.com>
2018-02-20 14:51:12 +0200

    fix nav bar, add all up toggle

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,53 +1,35 @@
-angular.module( 'app.home', [
+angular.module('app.home', [
   'ui.router'
 ])
 
-.config(function config( $stateProvider ) {
+.config(function config ($stateProvider) {
   $stateProvider
-    .state( 'home', {
+    .state('home', {
       url: '/',
       views: {
-        "images": {
+        'images': {
           controller: 'ImagesCtrl',
           templateUrl: 'images/images.tpl.html'
         },
-        "containers": {
+        'containers': {
           controller: 'ContainersCtrl',
           templateUrl: 'containers/containers.tpl.html'
         }
       }
     })
-    .state( 'about', {
-      url: '/about',
-      views: {
-        "about": {
-          controller: 'AboutCtrl',
-          templateUrl: 'home/home.tpl.html'
-        }
-      }
-    })
-  ;
 })
 
-.controller( 'AboutCtrl', 
-  function AboutCtrl( $scope, $uibModal ) {
-
-})
-
-.filter( 'filesize', function () {
-  var units = ['bytes','KB','MB','GB'];
-  return function( bytes, precision ) {
-    if ( isNaN( parseFloat( bytes )) || ! isFinite( bytes ) || bytes === 0 ) {
-      return '';
+.filter('filesize', function () {
+  var units = ['bytes', 'KB', 'MB', 'GB']
+  return function (bytes, precision) {
+    if (isNaN(parseFloat(bytes)) || !isFinite(bytes) || bytes === 0) {
+      return ''
     }
-    var unit = 0;
-    while ( bytes >= 1024 ) {
-      bytes /= 1024;
-      unit ++;
+    var unit = 0
+    while (bytes >= 1024) {
+      bytes /= 1024
+      unit++
     }
-    return bytes.toFixed( + precision ) + ' ' + units[ unit ];
-  };
+    return bytes.toFixed(+precision) + ' ' + units[ unit ]
+  }
 })
-
-;
-
